905978678630f1230ad622951f529dc831a1020c
aswin7469 <aswinas@pixysos.com>
2023-01-07 12:38:56 +0530

    sepolicy: adapt for pixys
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -79,3 +79,5 @@ ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
       BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test/mst_test_app
     endif
 endif
+
+-include device/pixys/sepolicy/qcom/sepolicy.mk
